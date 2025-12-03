9232f56d6af26020f3488fff8f8d7b02dcac8857
rbattezzati <rafael.battezzati@gmail.com>
2025-12-03 11:41:23 +0100

    initial tests

--- src/test/java/com/rbattezzati/carrental/service/CarRentalServiceTest.java
+++ src/test/java/com/rbattezzati/carrental/service/CarRentalServiceTest.java
@@ -10,7 +10,7 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CarRentalServiceTest {
 
@@ -31,16 +31,37 @@ public class CarRentalServiceTest {
 
     @Test
     void canReserveSedanWhenAvailable() {
-        LocalDateTime startDateTime = LocalDateTime.now().plusDays(1);
+        LocalDateTime start = LocalDateTime.now().plusDays(1);
+        Reservation reservation = carRentalService.reserveCar(CarType.SEDAN, start, 3);
 
-        Reservation reservation = carRentalService.reserveCar(CarType.SEDAN, startDateTime, 3);
         assertNotNull(reservation);
+        assertEquals(CarType.SEDAN, reservation.getCarType());
+        assertEquals(3, reservation.getDays());
+    }
 
+    @Test
+    void cannotExceedInventoryForOverlappingReservations() {
+        LocalDateTime start = LocalDateTime.now().plusDays(1);
 
+        // There are 2 sedans; make 2 overlapping reservations
+        carRentalService.reserveCar(CarType.SEDAN, start, 3);
+        carRentalService.reserveCar(CarType.SEDAN, start.plusHours(1), 2);
 
-
+        // Third overlapping reservation should fail
+        assertThrows(IllegalStateException.class, () ->
+                carRentalService.reserveCar(CarType.SEDAN, start.plusHours(2), 1)
+        );
     }
 
 
+    @Test
+    void canReserveSameCarTypeForNonOverlappingPeriods() {
+        LocalDateTime start = LocalDateTime.now().plusDays(1);
 
+        Reservation r1 = carRentalService.reserveCar(CarType.SUV, start, 2);
+        Reservation r2 = carRentalService.reserveCar(CarType.SUV, start.plusDays(2), 2);
+
+        assertNotEquals(r1.getReservationId(), r2.getReservationId());
+    }
+    
 }
